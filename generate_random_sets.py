2fb6a0e7fab8d3dfd83d2813fa3473074fa6c742
Nicholas Jumaoas <nljumaoas@gmail.com>
2023-12-05 01:31:39 -0800

    don't think the the last commit went through

--- generate_random_sets.py
+++ generate_random_sets.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 from generate_clean_sets import split_dfs
 
-combined_valid_test_df = pd.concat(split_dfs[1], split_dfs[2], ignore_index = True)
+combined_valid_test_df = pd.concat([split_dfs[1], split_dfs[2]], ignore_index = True)
 
 def generate_random_set(n, filename):
     # max n is 19478 (combined rows of valid + test)
     # produces csv file, returns DataFrame
     random_set_df = combined_valid_test_df.sample(n)
-    random_set_df.to_csv(filename, index=False)
+    random_set_df.to_csv(f"{filename}.csv", index=False)
     return random_set_df
